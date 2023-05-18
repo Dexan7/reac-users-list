1d330281c39d5f1a64f50c46656795b90f37a8a7
Dmytro Khan <dmytro.khan@sigma.software>
2023-05-18 20:48:58 +0300

    feat: change useState for useRef

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -2,41 +2,38 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css"
-import {useState} from "react";
+import {useState, useRef} from "react";
 import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const ageInputRef = useRef();
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0)  {
+    const enteredUserAge = ageInputRef.current.value;
+    if (enteredUsername.trim().length === 0 || enteredUserAge.trim().length === 0)  {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
       });
       return;
     }
-    if (+enteredAge < 0) {
+    if (+enteredUserAge < 0) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
+    props.onAddUser(enteredUsername, enteredUserAge);
     setEnteredUsername('');
-    setEnteredAge('');
+    ageInputRef.current.value = '';
   };
 
   const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
   };
 
   const errorHandler = () => {
@@ -57,7 +54,7 @@ const AddUser = (props) => {
           <label htmlFor="age">
             Age (years)
           </label>
-          <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
+          <input ref={ageInputRef} id="age" type="number" />
           <Button type="submit">
             Add user
           </Button>
